2ee05ec91dc859df9cde54c14081799283cbacc2
at-ynguyen <y.nguyen@asiantech.vn>
2021-03-17 09:30:11 +0700

    Initial Commit

---
+++ testdrone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testdrone
+//
+//  Created by Y Nguyen C. on 3/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testdrone/testdroneApp.swift
@@ -0,0 +1,17 @@
+//
+//  testdroneApp.swift
+//  testdrone
+//
+//  Created by Y Nguyen C. on 3/17/21.
+//
+
+import SwiftUI
+
+@main
+struct testdroneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
